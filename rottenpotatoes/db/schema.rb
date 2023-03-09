a6b3f035598f4d6acd2f1239f98344fa8503467d
Zhang Huixin <rzxyxxzhx1103@gmail.com>
2023-03-08 20:25:17 -0600

    part 1

--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2011_11_19_180638) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_020814) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2011_11_19_180638) do
     t.datetime "release_date", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
   end
 
 end
